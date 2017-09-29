dba020f7164cf2472b457e6d7812ff41d14113ae
10537 <yosefeliyahu@gmail.com>
2017-09-28 20:50:41 -0500

    [IMP]Adds Typescript_exercises folder and snake_exercise.

---
+++ typescript_exercises/tsSetup/dist/main.js
@@ -0,0 +1,7 @@
+var SweetSweetClass = (function () {
+    function SweetSweetClass() {
+        console.log("Even more sweeter");
+    }
+    return SweetSweetClass;
+}());
+var basil = new SweetSweetClass();
---
+++ typescript_exercises/tsSetup/main.ts
@@ -0,0 +1,7 @@
+class SweetSweetClass {
+  constructor () {
+    console.log("Even more sweeter")
+  }
+}
+
+let basil = new SweetSweetClass()
