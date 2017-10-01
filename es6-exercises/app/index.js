e9a9db9d0862982d84e87706061b0d03a5cd8fa3
10537 <yosefeliyahu@gmail.com>
2017-09-30 22:47:51 -0500

    [IMP]Adds new project ES6 for exercises. [IMP]Adds code exercices with filter and map. [IMP]Adds code exercices with arrow functions.

---
+++ es6-exercises/app/index.js
@@ -0,0 +1,40 @@
+function butter(...b) {
+  let a = [1, 2, 3, ...b];
+  return a;
+}
+
+console.log(butter(4, 5, 6));
+
+//Anonymous function examples
+
+//function blastoff(){
+//  console.log('1...2...3...blastoff');
+//}
+
+setTimeout(() => {
+  console.log('1...2...3...blastoff');
+}, 1000);
+
+const blastoff = () => {
+  console.log('1...2...3...blastoff');
+}
+
+blastoff();
+
+// Map with arrow function
+let points = [10, 20, 30];
+points = points.map(element => element + 1);
+console.log(points)
+
+// Filtering with arrow function
+let score = [90, 45, 67, 99, 87, 70];
+/*
+let isPassing = (grade) => {
+  return grade >= 70;
+}
+let passing = score.filter(isPassing);
+console.log(passing);
+*/
+
+let passing = score.filter(element => element >= 70)
+console.log(passing);
