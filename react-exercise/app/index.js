798371e8d1f017f10ed9aa35fc16bcdde474ab59
10537 <yosefeliyahu@gmail.com>
2017-10-01 19:31:37 -0500

    [IMP] Added exercise with React using a API for get the weather of a city.

--- react-exercise/app/index.js
+++ react-exercise/app/index.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+const key = "170261d3df4ed0262a39e8eee3ee4b01";
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      city: 'Medellin',
+      country_code: 'CO',
+    }
+  }
+
+  componentDidMount(){
+    this.grabWeather(this.state.city, this.state.country_code);
+  }
+
+  grabWeather(city, country_code){
+      fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${key}&q=${city},${country_code}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          description: json.weather[0].description
+        })
+      });
+  }
   render() {
     return (
-      <div>React Js and JSX in action!</div>
+      <div>
+        <h1>Weather for {this.state.city}, {this.state.country_code}</h1>
+        <h2>{this.state.description}</h2>
+      </div>
     )
   }
 }
