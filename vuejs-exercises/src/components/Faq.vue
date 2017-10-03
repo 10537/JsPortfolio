5173d0286930e7548a75fa9d2f1895b16eb2348b
10537 <yosefeliyahu@gmail.com>
2017-10-02 23:45:46 -0500

    [IMP]Added vuejs exercise project.

---
+++ vuejs-exercises/src/components/Faq.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="faq">
+    FAQ
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'faq',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+  @import '../mq'
+</style>
---
+++ vuejs-exercises/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+  @import '../mq'
+</style>
